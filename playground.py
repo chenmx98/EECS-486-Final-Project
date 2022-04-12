bce86fb4748ca2276b42d7619d18e377d5f83446
chenmx98 <xingchenming@outlook.com>
2022-04-12 16:56:42 -0400

    elec_dict

--- playground.py
+++ playground.py
@@ -1,18 +1,24 @@
-import twitterbot
+# import twitterbot
 # import geocoder
-import process
+# import process
 import pandas as pd
+import predict
 
 # cs_1 = "Chicago, IL"
 # g = geocoder.google('Mountain View, CA')
 # print(g.json)
 
-state = "New York"
-fips_df = pd.read_csv("fips2county.tsv", sep='\t')
-state_abbr = fips_df.loc[fips_df["StateName"] == state]["StateAbbr"].iloc[0]
-sc = "AL | BIBB"
-fips = fips_df.loc[fips_df["STATE_COUNTY"] == sc]["CountyFIPS"].iloc[0]
-
-
-state_abbr = fips_df.loc[fips_df["StateName"] == "District of Columbia"]["StateAbbr"].iloc[0]
-print(state_abbr)
+# state = "New York"
+# fips_df = pd.read_csv("fips2county.tsv", sep='\t')
+# state_abbr = fips_df.loc[fips_df["StateName"] == state]["StateAbbr"].iloc[0]
+# sc = "AL | BIBB"
+# fips = fips_df.loc[fips_df["STATE_COUNTY"] == sc]["CountyFIPS"].iloc[0]
+#
+#
+# state_abbr = fips_df.loc[fips_df["StateName"] == "District of Columbia"]["StateAbbr"].iloc[0]
+# print(state_abbr)
+#
+# filepath = "data/train/countypres_2000-2020.csv"
+# df = pd.read_csv(filepath, usecols=["county_fips", "party", "candidatevotes", "totalvotes"])
+# print(df.iloc[0]["county_fips"])
+predict.process_election_result()
